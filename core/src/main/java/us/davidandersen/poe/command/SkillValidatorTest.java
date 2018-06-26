e40d8be240a6e1a04e520fd97b5f7b8f85869bd2
David Andersen <git@davidandersen.us>
2018-06-26 14:59:46 -0700

    remove nonexistant method

--- core/src/main/java/us/davidandersen/poe/command/SkillValidatorTest.java
+++ core/src/main/java/us/davidandersen/poe/command/SkillValidatorTest.java
@@ -2,8 +2,6 @@ package us.davidandersen.poe.command;
 
 import org.junit.jupiter.api.Test;
 import poe.command.SkillValidator;
-import poe.entity.PassiveSkill.PassiveSkillBuilder;
-import poe.entity.StatValue.StatBuilder;
 import poe.repository.InMemoryPassiveSkillRepository;
 import poe.repository.InMemoryStatRepository;
 
@@ -15,13 +13,13 @@ class SkillValidatorTest
 		final InMemoryStatRepository statRepo = new InMemoryStatRepository();
 		final InMemoryPassiveSkillRepository passiveSkillRepo = new InMemoryPassiveSkillRepository();
 
-		passiveSkillRepo.create(PassiveSkillBuilder.passiveSkill()
-				.withName("a")
-				.withStats(
-						StatBuilder.stat()
-								.withDescription("a1"),
-						StatBuilder.stat()
-								.withDescription("a2")));
+		// passiveSkillRepo.create(PassiveSkillBuilder.passiveSkill()
+		// .withName("a")
+		// .withStats(
+		// StatBuilder.stat()
+		// .withDescription("a1"),
+		// StatBuilder.stat()
+		// .withDescription("a2")));
 
 		final SkillValidator command = new SkillValidator(passiveSkillRepo, statRepo);
 		command.execute();

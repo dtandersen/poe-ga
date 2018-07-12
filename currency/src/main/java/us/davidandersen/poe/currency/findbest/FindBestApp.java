3ec8eb188981619bbb95032019eb6af4d43a2c76
David Andersen <git@davidandersen.us>
2018-07-12 15:20:12 -0700

    delete old

--- currency/src/main/java/us/davidandersen/poe/currency/findbest/FindBestApp.java
+++ currency/src/main/java/us/davidandersen/poe/currency/findbest/FindBestApp.java
@@ -1,6 +1,5 @@
 package us.davidandersen.poe.currency.findbest;
 
-import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
@@ -13,7 +12,6 @@ import us.davidandersen.poe.currency.command.FindBest.FindBestRequest;
 import us.davidandersen.poe.currency.command.FindBest.FindBestResult;
 import us.davidandersen.poe.currency.command.Trade;
 import us.davidandersen.poe.currency.entity.Currency;
-import us.davidandersen.poe.currency.entity.Ratio;
 import us.davidandersen.poe.currency.repository.PriceRepository;
 
 @SpringBootApplication
@@ -34,11 +32,11 @@ public class FindBestApp implements CommandLineRunner
 	public void run(final String... args) throws Exception
 	{
 		final Currency want = Currency.CHAOS;
-		final Currency have = Currency.CHAOS;
-		final FindBest command = new FindBest(ratioRepository);
-		final int quantity = 10;
-		final int maxTrades = 2;
+		final Currency have = Currency.CHROMATIC;
+		final float quantity = 9.861112f;
+		final int maxTrades = 1;
 
+		final FindBest command = new FindBest(ratioRepository);
 		command.setRequest(new FindBestRequest() {
 			@Override
 			public String getWant()
@@ -76,72 +74,5 @@ public class FindBestApp implements CommandLineRunner
 			}
 		});
 		command.execute();
-	}
-
-	private void old() throws IOException
-	{
-		final List<Currency> currencies = List.of(
-				Currency.WISDOM,
-				Currency.PORTAL,
-				Currency.WHETSTONE,
-				Currency.ARMOURERS,
-				Currency.BAUBLE,
-				Currency.GEMCUTTER,
-				Currency.CHISEL,
-				Currency.TRANSMUTATION,
-				Currency.ALTERATION,
-				Currency.CHANCE,
-				Currency.REGAL,
-				Currency.ALCHEMY,
-				Currency.CHAOS,
-				Currency.BLESSED,
-				Currency.AUGMENTATION,
-				Currency.JEWELLER,
-				Currency.FUSING,
-				Currency.CHROMATIC,
-				Currency.SILVER,
-				Currency.SCOURING,
-				Currency.REGRET,
-				Currency.VAAL);
-
-		final Currency start = Currency.CHAOS;
-		final Currency end = Currency.CHAOS;
-		final float q1 = 100;
-		float best = 0;
-		Currency bestcur = null;
-		for (final Currency middle : currencies)
-		{
-			if (start == middle || end == middle)
-			{
-				continue;
-			}
-			Currency have = start;
-			Currency want = middle;
-			final Ratio ratioStartMiddle = ratioRepository.get(want, have);
-			if (ratioStartMiddle == null)
-			{
-				continue;
-			}
-			final float q2 = q1 / ratioStartMiddle.getPrice();
-			///////////////
-			want = end;
-			have = middle;
-			final Ratio ratioMiddleEnd = ratioRepository.get(want, have);
-			if (ratioMiddleEnd == null)
-			{
-				continue;
-			}
-			final float q3 = q2 / ratioMiddleEnd.getPrice();
-			System.out.println("current = " + start + ", " + middle + ", " + end + ", " + q3);
-			if (q3 > best)
-			{
-				best = q3;
-				bestcur = middle;
-				System.out.println("best = " + start + ", " + bestcur + ", " + end + ", " + best);
-			}
-		}
-
-		System.out.println("original = " + start + ", " + end + ", " + (q1 / ratioRepository.get(end, start).getPrice()));
-		System.out.println("best = " + start + ", " + bestcur + ", " + end + ", " + best);
 	}
 }
